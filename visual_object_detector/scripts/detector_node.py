846be42e5a97927b575092db162d2936c9f34ba2
Juan <juanpablo.rodriguezgomez@gmail.com>
2015-04-21 11:41:04 +0200

    solved code errors

--- visual_object_detector/scripts/detector_node.py
+++ visual_object_detector/scripts/detector_node.py
@@ -18,9 +18,8 @@ def detect_object(cv_frame):
 
 def handle_detect_object(req):
     """Return a response with value -1 if an error occured else the value returned by the object detector (a positive integer)"""
-
     try:
-        cv_frame = cv_bridge.imgmsg_to_cv(image_data)
+        cv_frame = cv_bridge.imgmsg_to_cv2(image_data)
         detection=detect_object(cv_frame)
         return DetectObjectResponse(detection)
     except Exception as e:
@@ -28,14 +27,16 @@ def handle_detect_object(req):
         return DetectObjectResponse(-1)
 
 def new_frame(data):
+    global image_data
     image_data=data
 
 def main():
     rospy.init_node('detect_object_node')
     service = rospy.Service('detect_object', DetectObject, handle_detect_object)
     subscriber=rospy.Subscriber('input_image',Image,new_frame,queue_size=1)
+    global cv_bridge    
     cv_bridge = CvBridge()
-    print "Ready to detect object."
+    print "Ready to detect object with ",cv_bridge
     rospy.spin()
 
 if __name__ == "__main__":
